1e782681865bbbc099729549b71438144a1c634f
andyDevMdg <andyavotiana@gmail.com>
2024-06-17 15:56:49 +0300

    feat: add getLocationByName

--- src/main/java/andy/dev/labs/location/Walker.java
+++ src/main/java/andy/dev/labs/location/Walker.java
@@ -5,16 +5,24 @@ import java.util.*;
 public class Walker {
 
 
+    public List<Location> walk(StreetMap streetMap, String startName, String destinationName) {
+        Location start = streetMap.getLocationByName(startName);
+        Location destination = streetMap.getLocationByName(destinationName);
+        if (start == null || destination == null) {
+            return new ArrayList<>();
+        }
+        return walk(streetMap, start, destination);
+    }
+
     public List<Location> walk(StreetMap streetMap, Location start, Location destination) {
         List<Location> path = new ArrayList<>();
         Set<Location> visitedLocations = new HashSet<>();
         if (walkProcess(start, destination, visitedLocations, path)) {
             return path;
         }
-        return new ArrayList<>(); // Return an empty path if no path is found
+        return new ArrayList<>();
     }
 
-    // Depth-First Search (DFS)
     private boolean walkProcess(Location currentLocation, Location destination, Set<Location> visitedLocations, List<Location> path) {
         path.add(currentLocation);
         visitedLocations.add(currentLocation);
