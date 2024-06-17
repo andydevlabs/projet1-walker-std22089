1a4c6d19d24cfda50801a589b0e6f0608a368042
andyDevMdg <andyavotiana@gmail.com>
2024-06-17 15:57:32 +0300

    refactor: delete unecessary lines of codes

--- src/main/java/andy/dev/labs/Main.java
+++ src/main/java/andy/dev/labs/Main.java
@@ -8,8 +8,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        StreetMap streetMap = new StreetMap();
-
         Location HEI = new Location("HEI");
         Location Pullman = new Location("Pullman");
         Location Balancoire = new Location("Balancoire");
@@ -17,7 +15,9 @@ public class Main {
         Location ESTI = new Location("ESTI");
         Location Boulevard = new Location("Boulevard");
         Location Marais = new Location("Marais");
+        Location Nexta = new Location("Nexta");
 
+        StreetMap streetMap = new StreetMap();
         streetMap.addLocation(HEI);
         streetMap.addLocation(Pullman);
         streetMap.addLocation(Balancoire);
@@ -25,6 +25,7 @@ public class Main {
         streetMap.addLocation(ESTI);
         streetMap.addLocation(Boulevard);
         streetMap.addLocation(Marais);
+        streetMap.addLocation(Nexta);
 
         streetMap.connectLocations("Andriatsihoarana", "HEI", "Pullman");
         streetMap.connectLocations("Andriatsihoarana", "Pullman", "HEI");
@@ -34,21 +35,20 @@ public class Main {
         streetMap.connectLocations("HEIToBalancoire", "Balancoire", "HEI");
         streetMap.connectLocations("Ranaivo", "Pullman", "Balancoire");
         streetMap.connectLocations("Ranaivo", "Balancoire", "Pullman");
-        streetMap.connectLocations("UnnamedStreet", "Balancoire", "ESTI");
-        streetMap.connectLocations("UnnamedStreet", "ESTI", "Balancoire");
+        streetMap.connectLocations("BalancoireESTI", "Balancoire", "ESTI");
+        streetMap.connectLocations("BalancoireESTI", "ESTI", "Balancoire");
         streetMap.connectLocations("BoulevardESTI", "Boulevard", "ESTI");
         streetMap.connectLocations("BoulevardESTI", "ESTI", "Boulevard");
         streetMap.connectLocations("BalancoireBoulevard", "Balancoire", "Boulevard");
         streetMap.connectLocations("BalancoireBoulevard", "Boulevard", "Balancoire");
         streetMap.connectLocations("MASE", "Marais", "Sekolintsika");
         streetMap.connectLocations("MASE", "Sekolintsika", "Marais");
-
-
+        streetMap.connectLocations("PUNE", "Pullman", "Nexta");
+        streetMap.connectLocations("PUNE", "Nexta", "Pullman");
 
         Walker walker = new Walker();
-
-        Location origin = streetMap.getLocationByName("Marais");
-        Location destination = streetMap.getLocationByName("Boulevard");
+        String origin = "Marais";
+        String destination = "Boulevard";
         List<Location> path = walker.walk(streetMap, origin, destination);
         System.out.println("Path: " + path);
     }
